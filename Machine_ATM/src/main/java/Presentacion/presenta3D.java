5c5984b9dd3390db69255c288e497dd226d1cc7a
Magucho <edgar.gomez@unicafam.edu.co>
2023-06-16 22:15:24 -0500

    Se logra acabar la presentación.

--- Machine_ATM/src/main/java/Presentacion/presenta3D.java
+++ Machine_ATM/src/main/java/Presentacion/presenta3D.java
@@ -1,19 +1,26 @@
 package Presentacion;
 
+import com.sun.j3d.loaders.IncorrectFormatException;
+import com.sun.j3d.loaders.ParsingErrorException;
+import com.sun.j3d.loaders.Scene;
+import com.sun.j3d.loaders.objectfile.ObjectFile;
 import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
-import com.sun.j3d.utils.image.TextureLoader;
+import com.sun.j3d.utils.image.ImageException;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import javax.media.j3d.*;
 import javax.swing.*;
-import javax.vecmath.Color3f;
 import javax.vecmath.Point3d;
-import javax.vecmath.Point3f;
 import javax.vecmath.Vector3d;
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 
 
 public class presenta3D extends JPanel {
+
     public presenta3D(){
+
         GraphicsConfiguration config= SimpleUniverse.getPreferredConfiguration();
         Canvas3D canvas3D= new Canvas3D(config);
 
@@ -23,8 +30,8 @@ public class presenta3D extends JPanel {
         SimpleUniverse universo= new SimpleUniverse(canvas3D);
         universo.getViewingPlatform().setNominalViewingTransform();
 
-     Vector3d posicionVista= new Vector3d();
-         posicionVista.z=2f;
+         Vector3d posicionVista= new Vector3d();
+         posicionVista.z=5f;
          posicionVista.x=0f;
          posicionVista.y=0f;
 
@@ -34,7 +41,7 @@ public class presenta3D extends JPanel {
 
          //rotacion
          Transform3D rotacion= new Transform3D();
-         rotacion.rotX(Math.toRadians(80));
+         rotacion.rotX(Math.toRadians(-10));
          rotacion.mul(transforVista);
 
          //universo.getViewingPlatform().getViewPlatformTransform().setTransform(transforVista);
@@ -49,82 +56,53 @@ public class presenta3D extends JPanel {
     public BranchGroup crearPincel(){
         BranchGroup pincel= new BranchGroup();
 
-        int []puntosRed= new int[1]; //rectangle
-        puntosRed[0]=4;
-        TriangleFanArray cuadrado= new TriangleFanArray(4,TriangleFanArray.COORDINATES|
-                                       TriangleFanArray.COLOR_3,puntosRed);
-        cuadrado.setCoordinate(0, new Point3f(-0.5f,0.5f,0f));
-        cuadrado.setCoordinate(1, new Point3f(0.5f,0.5f,0f));
-        cuadrado.setCoordinate(2, new Point3f(0.5f,-0.5f,0f));
-        cuadrado.setCoordinate(3, new Point3f(-0.5f,-0.5f,0f));
-
-        int []puntos= new int[1];//rectangulo2
-        puntos[0]=4;
-        TriangleFanArray rectangulo= new TriangleFanArray(4,TriangleFanArray.COORDINATES|
-                                         TriangleFanArray.COLOR_3,puntos);
-        rectangulo.setCoordinate(0, new Point3f(-0.3f,0.7f,0.01f));
-        rectangulo.setCoordinate(1, new Point3f(0.3f,0.7f,0.01f));
-        rectangulo.setCoordinate(2, new Point3f(0.3f,0.3f,0.01f));
-        rectangulo.setCoordinate(3, new Point3f(-0.3f,0.3f,0.01f));
-        for(int i=0; i<4;i++) rectangulo.setColor(i, new Color3f(Color.CYAN));
-
-        int []escalerab= new int[1];//base de escalera
-        escalerab[0]=4;
-        TriangleFanArray bescalera= new TriangleFanArray(4,TriangleFanArray.COORDINATES|
-                                        TriangleFanArray.COLOR_3,escalerab);
-        bescalera.setCoordinate(0, new Point3f(-0.15f,0.3f,0.01f));
-        bescalera.setCoordinate(1, new Point3f(0.15f,0.3f,0.01f));
-        bescalera.setCoordinate(2, new Point3f(0.15f,0.2f,0.01f));
-        bescalera.setCoordinate(3, new Point3f(-0.15f,0.2f,0.01f));
-        for(int i=0; i<4;i++) bescalera.setColor(i, new Color3f(Color.CYAN));
-
-
         TransformGroup objetoGiro= new TransformGroup();
         objetoGiro.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         objetoGiro.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
         pincel.addChild(objetoGiro);
 
-        Appearance app= new Appearance();
-        app.setPolygonAttributes(new PolygonAttributes(PolygonAttributes.POLYGON_FILL,PolygonAttributes.CULL_NONE,0f));
-
-        objetoGiro.addChild(new Shape3D(cuadrado,app()));
-        objetoGiro.addChild(new Shape3D(rectangulo,app));
-        objetoGiro.addChild(new Shape3D(bescalera,app));
-
-        /*Alpha rotacionAlpha= new Alpha(-1,4000); //rota por si mismo
+        Alpha rotacionAlpha= new Alpha(-1,4000); //rota por si mismo
         RotationInterpolator rotacion= new RotationInterpolator(rotacionAlpha,objetoGiro);
         rotacion.setSchedulingBounds(new BoundingSphere());
-        pincel.addChild(rotacion);*/
+        pincel.addChild(rotacion);
 
-        MouseRotate mr= new MouseRotate(); //Move with mouse
+        /*MouseRotate mr= new MouseRotate(); //Move with mouse
         mr.setTransformGroup(objetoGiro);
         mr.setSchedulingBounds(new BoundingSphere(new Point3d(),1000f));
-        pincel.addChild(mr);
+        pincel.addChild(mr);*/
+
+        //carga de objeto 3D
+
+        TransformGroup obj= new TransformGroup();
+        Transform3D obj3D= new Transform3D();
+        obj.getTransform(obj3D);
+        obj3D.setScale(0.5);
+        obj.setTransform(obj3D);
+
+        objetoGiro.addChild(obj);
+
+        int flags = ObjectFile.RESIZE;
+        ObjectFile file= new ObjectFile(flags, (float) (49.0 * Math.PI/180.0));
+
+        Scene scene= null;
+        //String filename = "src\\main\\modelo\\intento.obj";
+        String filename= null;
+        //File archivo = new File(filename);
+
+        try{
+            scene= file.load(filename == null ? "src\\main\\modelo\\intento.obj" : filename);
+            //scene = file.load(archivo.toURI().toURL());
+            // src\\main\\readme\\modelo2.obj
+
+        }catch (FileNotFoundException | ParsingErrorException | IncorrectFormatException  | ImageException ex) {
+            System.out.println("La imagen no carga " + ex);
+            throw new RuntimeException(ex);
+        }
+
+        objetoGiro.addChild(scene.getSceneGroup());
+
         return pincel;
     }
-    Appearance app(){
-        Appearance apariencia= new Appearance();
-        TexCoordGeneration texCoor= new TexCoordGeneration(
-                TexCoordGeneration.OBJECT_LINEAR,TexCoordGeneration.TEXTURE_COORDINATE_2
-        );
-        apariencia.setTexCoordGeneration(texCoor);
 
-        String cara= "src/main/imagenes/cuadricula.jpg";
-
-        TextureLoader loader= new TextureLoader(cara,this);
-        ImageComponent2D imagen=  loader.getImage();
-        Texture2D textura= new Texture2D(Texture.BASE_LEVEL,Texture.RGBA,imagen.getWidth(),imagen.getHeight());
-        textura.setImage(0,imagen);
-        textura.setEnable(true);
-
-        textura.setMagFilter(Texture.BASE_LEVEL_LINEAR);
-        textura.setMinFilter(Texture.BASE_LEVEL_LINEAR);
-
-        apariencia.setTexture(textura);
-        apariencia.setPolygonAttributes(new PolygonAttributes(PolygonAttributes.POLYGON_FILL,PolygonAttributes.CULL_NONE,0f));
-        apariencia.setTextureAttributes(new TextureAttributes());
-
-        return apariencia;
-    }
 
 }
--- Machine_ATM/src/main/java/Principal/principal.java
+++ Machine_ATM/src/main/java/Principal/principal.java
@@ -9,12 +9,11 @@ public class principal {
 
     public static void main(String[] args){
         System.out.println("la concha de la lora");
-
         System.setProperty("sun.awt.noerasebackground","true");
         JFrame ventana= new JFrame("Mi ventana 3D ");
         presenta3D prncipal =  new presenta3D();
         ventana.add(prncipal);
-        //ventana.setUndecorated(true);
+        ventana.setUndecorated(true);
         ventana.setSize(900,700);
         ventana.setVisible(true);
         ventana.setLocationRelativeTo(null);
